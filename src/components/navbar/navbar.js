e188581a400a847e4559f41c1c9d33198b694063
Gabriel Salinas Szada <75760163+gass-git@users.noreply.github.com>
2025-09-27 19:19:30 +0200

    feat: adds an improved version of the ripple effect on click

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -7,10 +7,26 @@ export default function Navbar() {
   const links = ['home', 'skills', 'projects', 'activity', 'about', 'blog']
   const [selected, setSelected] = useState(links[0])
   const [playSound] = useSound(selectionSound, { volume: 0.9 })
+  const [showRipple, setShowRipple] = useState(false)
+  const [coords, setCoords] = useState({x: 0, y: 0})
 
-  function handleClick(link) {
+
+  function handleClick(link, nativeEvent) {
     setSelected(link)
     playSound()
+    rippleEffect(nativeEvent)
+  }
+
+  // TODO: explore the implementation of using "requestAnimationFrame" instead of the hacky setTimeout() 
+  // implementation for reseting the animation.
+
+  
+  function rippleEffect(nativeEvent){
+    // React first commits the “false” state, the browser notices the class was removed, and 
+    // when you re-add it, the CSS animation runs again.
+    setCoords({x: nativeEvent.offsetX, y: nativeEvent.offsetY})
+    setShowRipple(false)
+    setTimeout(() => {setShowRipple(true)},0) 
   }
 
   return (
@@ -20,8 +36,17 @@ export default function Navbar() {
               <div
                 key={link}
                 className={selected !== link || "selected"}
-                onClick={() => handleClick(link)}
+                onClick={(e) => handleClick(link, e.nativeEvent)}
               >
+                {
+                  showRipple && selected === link ? 
+                  <div 
+                    className='ripple' 
+                    style={{ left: `${coords.x}px`, top: `${coords.y}px` }}
+                  /> 
+                  :
+                  null
+                }
                 {link}
               </div>
             ))}
